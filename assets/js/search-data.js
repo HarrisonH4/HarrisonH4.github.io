6fe745554ac3ebe3e15b7d6b11fbfb656079c2d5
HarrisonH4 <145985397+HarrisonH4@users.noreply.github.com>
2026-07-14 11:57:52 +0000

    Deploying to gh-pages from @ HarrisonH4/HarrisonH4.github.io@62e4e17a1a6b5aba8d228339f558a06f90dee491 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,9 +430,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-base-form",
+          title: 'Base Form',
+          description: "PlayStation 5 Game Attempt",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
